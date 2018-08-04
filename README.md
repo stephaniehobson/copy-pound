8c0ca0c6cbdbc6cee2665b8c84a084cbfe034a2d
Stephanie Hobson <sjhobson@gmail.com>
2018-08-04 16:35:58 -0700

    Create extension

---
+++ README.md
@@ -0,0 +1,7 @@
+# Copy Pound
+
+## What it does ##
+
+This extension adds an icon to the toolbar. Clicking it pops up a short list of
+symbols, including the GBP or pound sterling. Clicking a symbol copies it to
+the clipboard.
---
+++ popup/copy.js
@@ -0,0 +1,37 @@
+var symbols = ['£','€','¥','¢','©','™','®','°C','°F','•'];
+var inputsDiv = document.querySelector('#popup-inputs');
+var buttonsDiv = document.querySelector('#popup-buttons');
+
+// loop over symbols and create inputs and buttons
+symbols.forEach(function(item, index, array) {
+    var inputId = 'input-' + index;
+
+    // input
+    var newInput = document.createElement('input');
+    newInput.id = inputId;
+    newInput.value = item;
+    newInput.className = 'hidden';
+    inputsDiv.appendChild(newInput);
+
+    // button
+    var newButton = document.createElement('button');
+    newButton.textContent = item;
+    buttonsDiv.appendChild(newButton);
+
+    // listener
+    newButton.addEventListener("click", function() {
+        copy(inputId);
+    });
+});
+
+function copy(inputId) {
+    console.log(inputId);
+    var copyText = document.querySelector('#' + inputId);
+    copyText.select();
+    document.execCommand("copy");
+    document.querySelector('body').className = 'copied';
+    window.setTimeout(function(){
+        window.close();
+    }, 1000);
+    // window.close();
+}
